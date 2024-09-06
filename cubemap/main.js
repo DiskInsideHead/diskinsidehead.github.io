f63feb9ea113727472253befda400fd8d92470b9
Disk Inside Head <dih.head@yandex.ru>
2024-09-06 20:59:32 +0300

    Signed-off-by: Disk Inside Head <dih.head@yandex.ru>

--- cubemap/main.js
+++ cubemap/main.js
@@ -169,7 +169,7 @@ function renderFace(data, faceName, position) {
     };
     worker.onmessage = setPreview;
     worker.postMessage(Object.assign({}, options, {
-        maxWidth: window.innerWidth * 0.20,
+        Width: window.innerHeight * 0.16,
         interpolation: 'linear',
     }));
     workers.push(worker);
